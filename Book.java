4793eb0335b24a29907f26080148ec6c0ff33e35
Emmanuel <emmanuelori00@gmail.com>
2026-06-16 09:46:01 -0400

    Adding the Book class file and updated the Main class file

---
+++ Book.java
@@ -0,0 +1,56 @@
+//Creating and defining the Book class
+public class Book {
+	
+	//Var for storing our books' unique IDs
+	private int id;
+	
+	//Var to store titles of books
+	private String title;
+	
+	//Var to store name of book authors
+	private String author;
+
+	//Bool to track whehter a book is available to borrow
+	private boolean available;
+
+
+	//Constructor for book objects
+	public Book (int id, String title, String author){
+
+	//Will store the id parameter as the book's id value
+	this.id= id;
+
+	//Will store the title parameter as the book's title
+	this.title= title;
+
+	//Will store author parameter as the book's author name
+	this.author = author;
+
+	//Setting new books as available
+	this.available = true;
+	}
+
+	//Method to return the title of a book
+	public String getTitle() {
+	
+		return title;
+	}
+	
+	// Returns whether or not the book is available
+	public boolean isAvailable() {
+	
+	return available;
+	}
+
+	//MArks book as borrowed by setting availability to false
+	public void borrowBook() {
+	
+	available = false;
+	}
+	
+	//Marks book as returned by setting availability to true
+	public void returnBook () {
+	
+	available = true;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+	
+	//Creating a book object
+	Book book1 = new Book( 1, "The Hobbit", "J.R.R Tolkien");
+
+	//Print out the name of the book
+	System.out.println(book1.getTitle());
+	}
+} 
