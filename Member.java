7cbc6d0a8bd65f41da6e08625a08cf529ace8a0d
Emmanuel <emmanuelori00@gmail.com>
2026-06-24 13:22:31 -0400

    Added code for the member class

---
+++ Member.java
@@ -0,0 +1,16 @@
+public class Member {
+	private int memberId;
+	private String name;
+	private String email;
+	private String username;
+	private String password;
+
+	public Member(int memberId, String name, String email, String username, String password) {
+		this.memberId = memberId;
+		this.name= name;
+		this.email= email;
+		this.username= username;
+		this.password= password;
+	}
+}
+
