8b77a5a11f148bf1f9618ffe3e8bee2a34f82364
flash2005mc-sketch <flash2005mc@gmail.com>
2026-07-06 16:27:10 -0400

    db name fix

--- src/DBConn.java
+++ src/DBConn.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConn {
-    private static final String URL      = "jdbc:mysql://localhost:3306/dewey";
+    private static final String URL      = "jdbc:mysql://localhost:3306/librarydb";
     private static final String USER     = "root";
     private static final String PASSWORD = "password";
 
