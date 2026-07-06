da0fe9e3a20b17ab38d9dbc5bee4a6552e4dc5c4
Emmanuel <emmanuelori00@gmail.com>
2026-07-06 19:53:33 -0400

    Reorganize JavaFX UI and build librarian dashboard

--- src/App.java
+++ src/App.java
@@ -7,14 +7,27 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui/Dashboard.fxml"));
-        Scene scene = new Scene(loader.load(), 900, 600);
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("ui/librarian/LibrarianDashboard.fxml")
+        );
 
-    scene.getStylesheets().add(
-        getClass().getResource("css/dashboard.css").toExternalForm()
-    );
+        Scene scene = new Scene(loader.load(), 1440, 900);
+
+        scene.getStylesheets().add(
+                getClass().getResource("css/shared/base.css").toExternalForm()
+        );
+
+        scene.getStylesheets().add(
+                getClass().getResource("css/shared/components.css").toExternalForm()
+        );
+
+        scene.getStylesheets().add(
+                getClass().getResource("css/librarian/librarian-dashboard.css").toExternalForm()
+        );
 
         stage.setTitle("Library Management System");
+        stage.setMinWidth(1100);
+        stage.setMinHeight(750);
         stage.setScene(scene);
         stage.show();
     }
